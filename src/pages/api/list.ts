dda8269637e25ea7a0861f67d1c961e2c295be44
Aidan Timson <contact@timmo.xyz>
2023-03-18 13:34:18 +0000

    Update

--- src/pages/api/list.ts
+++ src/pages/api/list.ts
@@ -6,11 +6,11 @@ import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+const openai = new OpenAIApi(configuration);
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ListModelsResponse>
 ) {
-  const openai = new OpenAIApi(configuration);
   res.status(200).json((await openai.listModels()).data);
 }
